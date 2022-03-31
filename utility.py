44d6ab0e78fe30edd19be16fad2f58ccd7133283
Lachlan Ross <lachlan.f.ross@gmail.com>
2022-03-31 17:07:42 +0800

    d

--- utility.py
+++ utility.py
@@ -27,7 +27,7 @@ config = {  # initialising database connection
 }
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
-unique_id = 'ella/'  # heroku, lach, tom, cozza
+unique_id = 'heroku/'  # heroku, lach, tom, cozza
 
 
 ADD_MESSAGE_L = 'trade_results/message_count/'
@@ -319,39 +319,6 @@ def binance_timestamp_local(timestamp):
     timedelta = 60 * 60 * 8  # + 8 Hours from UTC
     timestamp = datetime.timestamp(utctime) + timedelta
     return int(timestamp * 1000)
-
-
-class Sleeper:
-    # Group sleep calls allowing instant cancellation of all
-    def __init__(self, loop):
-        self.loop = loop
-        self.tasks = set()
-
-    async def sleep(self, delay, result=None):
-        coro = aio.sleep(delay, result=result, loop=self.loop)
-        task = aio.ensure_future(coro)
-        self.tasks.add(task)
-        try:
-            return await task
-        except aio.CancelledError:
-            return result
-        finally:
-            self.tasks.remove(task)
-
-    def cancel_all_helper(self):
-        # Cancel all pending sleep tasks
-        cancelled = set()
-        for task in self.tasks:
-            if task.cancel():
-                cancelled.add(task)
-        return cancelled
-
-    async def cancel_all(self):
-        # Coroutine cancelling tasks
-        cancelled = self.cancel_all_helper()
-        await aio.wait(self.tasks)
-        self.tasks -= cancelled
-        return len(cancelled)
 
 
 
